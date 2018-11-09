7dfe58386c356df8fa9a03f23ba137af953e1b99
Tyler Kennedy <kennedytyler25@icloud.com>
2018-11-09 09:21:14 -0500

    Starting with functions like favoriting tweets

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import tweepy
 import json
 
-
+# Read the credentials needed to authenticate with the api from a JSON file so the credentials are not published on GitHub
 with open("twitterCredentials.json", "r") as read_file:
 	credentials = json.load(read_file)
 	read_file.close()
@@ -19,3 +19,15 @@ api = tweepy.API(auth)
 
 user = api.me()
 print (user.name)
+
+def mainFunction():
+	search = "Keyword"
+	numberOfTweets = "Number of tweets I am interacting with"
+	for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+		try:
+			tweet.favorite()
+			print('Favorited the tweet')
+		except tweepy.TweepError as e:
+			print(e.reason)
+		except StopIteration:
+			break
