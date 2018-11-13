0f63d9ed28cb5d8b024b99529534f42126a0f984
Tyler Kennedy <kennedytyler25@icloud.com>
2018-11-13 11:47:08 -0500

    Sent first tweet

--- test.py
+++ test.py
@@ -2,33 +2,29 @@ import tweepy
 import json
 
 # Read the credentials needed to authenticate with the api from a JSON file so the credentials are not published on GitHub
-with open("twitterCredentials.json", "r") as read_file:
-	credentials = json.load(read_file)
-	read_file.close()
+def getCredentials(fileLocation):
+	with open(fileLocation, "r") as read_file:
+		credentials = json.load(read_file)
+		read_file.close()
+	return credentials
 	
+def login_api():
+	consumer_key = getCredentials("twitterCredentials.json")['consumer_key']
+	consumer_secret = getCredentials("twitterCredentials.json")['consumer_secret']
+	access_token = getCredentials("twitterCredentials.json")['access_token']
+	access_token_secret = getCredentials("twitterCredentials.json")['access_token_secret']
 
-consumer_key = credentials['consumer_key']
-consumer_secret = credentials['consumer_secret']
-access_token = credentials['access_token']
-access_token_secret = credentials['access_token_secret']
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	return tweepy.API(auth)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-user = api.me()
-print (user.name)
-
-def mainFunction():
-	search = "Keyword"
-	numberOfTweets = "Number of tweets I am interacting with"
-	for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
-		try:
-			tweet.favorite()
-			print('Favorited the tweet')
-		except tweepy.TweepError as e:
-			print(e.reason)
-		except StopIteration:
-			break
+# Sends a tweet
+def main():
+	api = login_api()
+	tweet = "My first tweet"
+	status = api.update_status(status=tweet)
+	
+	
+if __name__ == "__main__":
+	main()
 			
-
